77160c6bcf58d02b2906e6c53182a4ebd1debea0
bryanbilek <you@example.com>
2020-03-27 10:41:24 -0500

    setup login, privateroute, axioswithauth files

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import PrivateRoute from "./components/PrivateRoute";
 import Login from "./components/Login";
+import BubblePage from "./components/BubblePage";
 import "./styles.scss";
 
 function App() {
--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,12 +1,58 @@
-import React from "react";
+import React, { useState } from "react";
+import { useForm } from "react-hook-form";
+import { axiosWithAuth } from "../utils/axiosWithAuth";
+import { useHistory } from "react-router-dom";
 
 const Login = () => {
   // make a post request to retrieve a token from the api
   // when you have handled the token, navigate to the BubblePage route
+
+  const [user, setUser] = useState({
+    username: "",
+    password: ""
+  });
+
+  const { register, handleSubmit, reset } = useForm();
+  const { history } = useHistory();
+
+  const handleChanges = e => {
+    setUser({...user, [e.target.name]: e.target.value });
+  };
+
+  const onSubmit = (data, e) => {
+    e.preventDefault();
+    axiosWithAuth()
+      .post("/api/login", data)
+      .then(res => {
+        console.log('***POST***', res);
+        localStorage.setItem("token", (res.data.payload));
+        history.push("/protected");
+      })
+      .catch(err => console.log(err.response));
+  };
   return (
     <>
-      <h1>Welcome to the Bubble App!</h1>
-      <p>Build a login page here</p>
+     <h1>Welcome to the Bubble App!</h1>
+
+      <form onSubmit={handleSubmit(onSubmit)}>
+      <input
+        type="text"
+        placeholder="Username"
+        name="username"
+        ref={register({ required: true, maxLength: 20 })}
+        onChange={handleChanges}
+      />
+
+      <input
+        type="password"
+        placeholder="Password"
+        name="password"
+        ref={register({ required: true, maxLength: 20 })}
+        onChange={handleChanges}
+      />
+
+      <button type="submit">Submit</button>
+    </form>
     </>
   );
 };
